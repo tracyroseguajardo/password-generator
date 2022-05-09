92bc0b159dc37481f952125993f7a30b81bb3409
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-05-09 16:47:08 -0700

    added array values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
+
+// character declarations
+
+var noCaps = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var allCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"]
+
+//test to make sure arrays work
+console.log(noCaps);
+console.log(allCaps);
+console.log(num);
+console.log(specialChar);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +25,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
