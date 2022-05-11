3c836a1c3004104fda916f6079012aae57d3ecef
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-05-10 21:20:33 -0700

    working selection of a single random character

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@
 
 // character declarations
 
-var noCaps = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var allCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"]
+const noCaps = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const allCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"]
 
 //test to make sure arrays work
 console.log(noCaps);
@@ -14,15 +14,6 @@ console.log(allCaps);
 console.log(num);
 console.log(special);
 
-//prompt practice: alert, confirm, prompt
-
-//alert("input number of characters between 8 and 128");
-
-//if (confirm("Press a button!")) {
-//  txt = "You pressed OK!";
-//} else {
-//  txt = "You pressed Cancel!";
-//}
 
 //var length = prompt("Please enter desired lenght of characters", "between 8-128");
 //let text;
@@ -38,8 +29,64 @@ if (length < 8 || length > 128) {
   alert("warning! invalid value! please refresh page and try again");
   //prompt("warning! invalid value! please enter desired password length between 8 and 128:", "re-enter value");
 } else {
+  alert("great! you selected: " + (length));
   console.log(length);
-  //prompt("would you like to include lower case letters?", enter "true" && "false");
+}
+
+var char1 = prompt("would you like to include lower case letters?", "enter true or false");
+if (char1 != "true" && char1 != "false") {
+  alert("warning! invalid answer!");
+  var char1 = prompt("would you like to include lower case letters?", "enter true or false");
+} else {
+  alert("great! you selected: " + char1);
+  console.log(char1)
+}
+
+var char2 = prompt("would you like to include upper case letters?", "enter true or false");
+if (char2 != "true" && char2 != "false") {
+  alert("warning! invalid answer!");
+  var char1 = prompt("would you like to include upper case letters?", "enter true or false");
+} else {
+  alert("great! you selected: " + char2);
+}
+
+var char3 = prompt("would you like to include numerals?", "enter true or false");
+if (char3 != "true" && char3 != "false") {
+  alert("warning! invalid answer!");
+  var char3 = prompt("would you like to include lower case letters?", "enter true or false");
+} else {
+  alert("great! you selected: " + char3);
+}
+
+var char4 = prompt("would you like to include special characters?", "enter true or false");
+if (char4 != "true" && char4 != "false") {
+  alert("warning! invalid answer!");
+  var char4 = prompt("would you like to include lower case letters?", "enter true or false");
+} else {
+  alert("great! you selected: " + char4);
+}
+
+//this combines true array to give character bank (not complete for now)
+if (char1 == "true" && char2 == "true") {
+  var characterBank = [].concat(noCaps, allCaps);
+  console.log(characterBank);
+  console.log(characterBank.length);
+}
+
+//this will produce an index selector for the number of characters in bank
+var index = Math.floor(Math.random() * (characterBank.length)) + 1;
+console.log(index);
+
+//types single selected character in console log!
+console.log(characterBank[(index)]);
+
+/*function randomIndex() {
+  return Math.floor(Math.random() * (characterBank.length - min+ 1) ) + min;
+}
+
+if (char1 == "true") {
+  a=1, b=25;
+ noCaps[function randomIndex()];
 }
 
 /*function passwordLength() {
